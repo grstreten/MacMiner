6c06d1e06a56630024172df39a79ef7e3a2bf060
George Streten <grstreten@me.com>
2014-06-21 10:33:38 +0100

    Initial Commit

---
+++ MacMiner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacMiner
+//
+//  Created by George Streten on 21/06/2014.
+//  Copyright (c) 2014 EVERYONE. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacMiner/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MacMiner
+//
+//  Created by George Streten on 21/06/2014.
+//  Copyright (c) 2014 EVERYONE. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
