b5ba9348257fe0731adda11f2bc5895d27194891
Niklas Vangerow <notrodash@gmail.com>
2014-06-21 16:12:33 +0100

    Moved the files
    Signed-off-by: Niklas Vangerow <notrodash@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import subprocess
 from datetime import datetime
-import separationengine as se
 import threading
 from flask import Flask, request, render_template
 
 def tcpdump_thread():
+    import separationengine as se
     print("It takes a while to do something, don't worry ;)")
     process = subprocess.Popen("/usr/sbin/tcpdump", stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
 
@@ -18,3 +18,14 @@ tcpdumpt = threading.Thread(target=tcpdump_thread)
 tcpdumpt.start()
 
 app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template("index", usage={"hours" : 9, "charger" : 21}, app={"mostused", "Goat Simulator"}, network={"analytics" : 476})
+
+@app.route("/data")
+def data():
+    return render_template("fulldata")
+
+app.debug = True
+app.run(host="0.0.0.0", port=6789)
